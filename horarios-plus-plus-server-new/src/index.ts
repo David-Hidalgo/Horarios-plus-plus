b5bd227d896be2c74d491b19b2dcf09328f4a8ac
David-Hidalgo <dehidalgo.22@est.ucab.edu.ve>
2024-06-28 10:27:48 -0400

    crear cl

--- horarios-plus-plus-server-new/src/index.ts
+++ horarios-plus-plus-server-new/src/index.ts
@@ -1,7 +1,7 @@
 import { Elysia } from "elysia";
 import mongoose, { type ConnectOptions } from "mongoose";
 import { cors } from "@elysiajs/cors";
-import {DBController} from "./controllers/db";
+import { DBController } from "./controllers/db";
 
 import { plugin } from "./routes/schedules.routes";
 import { pluginSession } from "./routes/sessions.routes";
@@ -9,35 +9,41 @@ import { pluginSection } from "./routes/sections.routes";
 import { pluginSubject } from "./routes/subjects.routes";
 import { pluginUser } from "./routes/user.routes";
 
-
 const username = encodeURIComponent("DanCas");
 const password = encodeURIComponent("queso");
 
-const uri = //"mongodb://127.0.0.1:27017/horariospp";															//Conexión a base de datos de manera local
-			`mongodb+srv://${username}:${password}@horariosplus.pktabwe.mongodb.net/?retryWrites=true&w=majority`; //Conexión a base de datos de manera remota
-	
+const uri =
+	//"mongodb://127.0.0.1:27017/horariospp";															//Conexión a base de datos de manera local
+	`mongodb+srv://${username}:${password}@horariosplus.pktabwe.mongodb.net/?retryWrites=true&w=majority`; //Conexión a base de datos de manera remota
+
 const clientOptions: ConnectOptions = {};
-const controladordb:DBController = await DBController.run(uri);
+const controladordb: DBController = await DBController.run(uri);
 
-const app = new Elysia()
-	.use(cors())
-	.get("/", () => "Hello Elysia")
-	.post("/hi", () => "hi")
-	.use(plugin({ prefix: "Schedules" }))
-	.use(pluginSession({ prefix: "Sessions" }, controladordb))
-	.use(pluginSection({ prefix: "Sections" }, controladordb))
-	.use(pluginSubject({ prefix: "Sections" }, controladordb))
-	.use(pluginUser({ prefix: "Sections" }, controladordb))
+class main {
+	public controller;
+	public dbController;
+	constructor(controladordb: DBController) {
+		this.dbController = controladordb;
+		const controlador = new Elysia()
+			.use(cors())
+			.get("/", () => "Hello Elysia")
+			.post("/hi", () => "hi")
+			.use(plugin({ prefix: "Schedules" }))
+			.use(pluginSession({ prefix: "Sessions" }, controladordb))
+			.use(pluginSection({ prefix: "Sections" }, controladordb))
+			.use(pluginSubject({ prefix: "Sections" }, controladordb))
+			.use(pluginUser({ prefix: "Sections" }, controladordb))
 
-	.onError(({ code }) => {
-		if (code === "NOT_FOUND") return "Route not found :{";
-	})
-	.listen(4000);
+			.onError(({ code }) => {
+				if (code === "NOT_FOUND") return "Route not found :{";
+			})
+			.listen(4000);
 
+		console.log(
+			`🦊 Elysia is running at ${controlador.server?.hostname}:${controlador.server?.port}`,
+		);
+		this.controller = controlador;
+	}
+}
 
-
-console.log(
-	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
-);
-
-export type App = typeof app;
+const app = new main(controladordb);
